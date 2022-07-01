6872779c283fdf50496ff5f1469c8bef29f9b23f
Elithon Silva <elithonsilva@gmail.com>
2022-07-01 17:14:17 -0300

    impl func que retorna Número de estudantes estudando no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,16 @@
-from typing import Tuple
-
-
 def study_schedule(permanence_period, target_time):
-    n = len(permanence_period)
-    if permanence_period != Tuple:
+    count_target_repeat = 0
+    if type(target_time) != int:
         return None
-    if target_time == None:
-        return None
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+        if period[0] <= target_time <= period[1]:
+            count_target_repeat += 1
 
-    return -1
+    return count_target_repeat
+
+
+# print(study_schedule(
+#     [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2
+# ))
