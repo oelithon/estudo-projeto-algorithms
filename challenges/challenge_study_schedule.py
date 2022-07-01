970df8c76284943d7544995349c1cd4b6b94631e
Elithon Silva <elithonsilva@gmail.com>
2022-07-01 15:11:03 -0300

    retorna None se em permanence_period houver alguma entrada inválida

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,10 @@
+from typing import Tuple
+
+
 def study_schedule(permanence_period, target_time):
     n = len(permanence_period)
+    if permanence_period != Tuple:
+        return None
     if target_time == None:
         return None
 
