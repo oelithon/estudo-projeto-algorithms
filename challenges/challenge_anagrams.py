756b440fdb1e54b93c81e5ab2c7dfae069f1cd69
Elithon Silva <elithonsilva@gmail.com>
2022-07-04 17:54:22 -0300

    Retorne True se as palavras passadas por parâmetro forem anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,14 @@
 def is_anagram(first_string, second_string):
+    string_one = list(first_string)
+    string_two = list(second_string)
 
     if first_string == '' or second_string == '':
         return False
 
+    for word in string_one:
+        if word in string_two:
+            string_two.remove(word)
+
+    if len(string_two) == 0:
+        return True
     return False
